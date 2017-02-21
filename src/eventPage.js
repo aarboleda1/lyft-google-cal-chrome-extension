277b360ecc58bcd475ab4e4211afba212205b9b0
Anton <anton.arboleda@gmail.com>
2017-02-20 17:28:15 -0800

    [update] - add click handler button for ride share

--- src/eventPage.js
+++ src/eventPage.js
@@ -1,26 +1,25 @@
-var body = document.getElementsByClassName('body');
-
+var body = document.querySelector('body');
 body.onclick = function () {
 	var events = document.getElementsByClassName("cbrd");
 	addRideScheduleButton(events);
 };
+
 function addRideScheduleButton (events) {
 	var eventModal = document.getElementsByClassName('neb-footer');
 	var button = document.createElement('BUTTON');
 	var buttonText = document.createTextNode('Schedule Ride');
+
 	button.appendChild(buttonText);	
 	eventModal[0].appendChild(button);	
-	// create modal with ride schedulign with Lyftt/Uber
+}
+
+function getLyftScreen () {
+	//make request to lyft API
+
+	//display in modal
 
 };
 
-function scheduleRide () {
-	//make call to API
-
-	//Display data on the screen
-
-};
-
-function confirmRide () {
+function scheduler () {
 
 };
