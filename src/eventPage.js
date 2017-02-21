09d6d284220022521188c105f34a6afd510313b7
Anton <anton.arboleda@gmail.com>
2017-02-20 16:51:32 -0800

    [update] - significant progress to lyft Javascript

--- src/eventPage.js
+++ src/eventPage.js
@@ -1,39 +1,26 @@
-var body = document.querySelector('body');
+var body = document.getElementsByClassName('body');
+
 body.onclick = function () {
-	var events = document.getElementsByClassName("evt-lk");
-	getEventList(events);
+	var events = document.getElementsByClassName("cbrd");
+	addRideScheduleButton(events);
+};
+function addRideScheduleButton (events) {
+	var eventModal = document.getElementsByClassName('neb-footer');
+	var button = document.createElement('BUTTON');
+	var buttonText = document.createTextNode('Schedule Ride');
+	button.appendChild(buttonText);	
+	eventModal[0].appendChild(button);	
+	// create modal with ride schedulign with Lyftt/Uber
+
 };
 
-function getEventList (events) {
-	var eventList = [];	
-	for (var event in events) {
-		console.log(events[event].innerHTML);
-		var listNode = document.createElement('LI');
-		// var text = eventList.push(events[event].innerHTML);			
-		// var textNode = document.createTextNode(text);
-		// document.getElementById("event-list").appendChild(listNode);
-		// console.log(textNode);
-	}
-	return eventList;
-}
+function scheduleRide () {
+	//make call to API
 
-document.getElementById('getEvents').onclick = function (e) {
-	console.log('hello world');
-	var events = document.getElementsByClassName("evt-lk");
-	var eList = getEventList(events);
-	console.log(eList);
-	document.body.innerHTML +=
-		'<ul><li>Hello world </li></ul>'
-	console.log('helo world');
-}
+	//Display data on the screen
 
+};
 
+function confirmRide () {
 
-// var events = document.getElementsByClassName("evt-lk");
-
-// for(var i = 0; i < events.length; i++) {
-// 	var event = events[i];
-// 	event.onclick = function(e) {
-// 		console.log("event", e);
-// 	}
-// }
+};
