b61cb82b08ee172d8a342d7d9b1d677b4eeb952f
Anton <anton.arboleda@gmail.com>
2017-04-06 14:43:52 -0700

    [update] - code cleanup

--- src/eventPage.js
+++ src/eventPage.js
@@ -1,70 +1,43 @@
-let log = console.log;
-function showModal () {
-
+function requestRide(origin, destination) {
+	console.log('requesting a ride!!');
+	// $.ajax({
+	//     url: 'https://api.lyft.com/v1/rides',
+	// 		beforeSend: function(xhr) { 
+	// 			xhr.setRequestHeader('Authorization','Bearer ' + 'gAAAAABYteP_LNYsphMTECGHw7p1s3zJKTeOTNIDn7-RGhhmTIyeyoT5pkanBkBVffuGkS-d6SSwi87Kabf2hoxyirfrcqvg3W0PERITkspqAUZ_PBzTUnjQ1E6sbfgWm4_a5xqgo0ZZV5h20Dy0VW7hvdriL8RMSuZqi7xyk-tndFCyWrcGmbs= ');
+	// 			xhr.setRequestHeader('Content-Type', 'application/json');
+	// 		}, 
+	// 		data: {ride_type : "lyft", 
+	// 			origin : {lat : origin.lat, lng : origin.lng },  // dynamically use origin.lat && origin.lng
+	// 			destination: {lat: destination.lat, lng: destination.lng} // dynamically use destination.lat destination.lng
+	// 		}, 
+	// 		type: 'POST',
+	// 		// contentType: 'application/json',
+	// })
+	// .done(function (res) {
+	// });
 }
 
-
-// window.opennewtab
-function lyftModal (button, eventModal) {
-	let lyftModal = document.createElement('DIV');
-	lyftModal.className += 'lyftModal';
-	lyftModal.setAttribute('id', eventModal[0].id);
-	lyftModal.style.display = 'none';
-	lyftModal.style.position = 'fixed';
-	lyftModal.style.paddingTop = '100px';
-	lyftModal.style.zIndex = '1';
-	lyftModal.style.top = '0';
-	lyftModal.style.left = '0';
-	lyftModal.style.width = '100%';
-	lyftModal.style.height = '100%';
-	lyftModal.style.backgroundColor = 'rgb(0,0,0)';
-	lyftModal.style.backgroundColor = 'rgba(0,0,0,0.4)';
-  
-	var span = document.createAttribute('SPAN');
-
-	var modalContent = document.createElement('DIV');
-	lyftModal.className += 'modal-content';
-	modalContent.style.backgroundColor = '#fefefe';
-	modalContent.style.margin = 'auto';
-	modalContent.style.padding = '20px';
-	modalContent.style.border = '1px solid #888';
-	modalContent.style.border = '80%';
-	lyftModal.appendChild(modalContent);
-	log(lyftModal, modalContent, 'IS THE STUFF');
-	setTimeout(showModal, 1000)	
-}
-
-function insertLyftButton (eventModal) {
-	var button = document.createElement('BUTTON');
-	var buttonText = document.createTextNode('Schedule Ride');
-	button.addEventListener('click', function () {
-		// lyftModal(this, eventModal);
-		// var lyft = 'https://www.lyft.com';
-		// window.open(lyft,'popUpWindow','height=400,width=600,left=10,top=10,,scrollbars=yes,menubar=no'); return false;
-
-		
-		console.log(window.token, 'TOEKKNENNAFEIAOJFOAIJEO');
-		$.ajax({
-        url: 'https://api.lyft.com/v1/rides',
-				beforeSend: function(xhr) { 
-    			xhr.setRequestHeader('Authorization','Bearer ' + window.token)
-    		}, 
-				data: '{"ride_type" : "lyft", "origin" : {"lat" : 37.7763, "lng" : -122.3918 } }', 
-				type: 'POST',
-				contentType: 'application/json',
-		})
-		.done(function (res) {
-			console.log(res);
-		});
-
-		// curl -X POST -H "Authorization: Bearer <access_token> " \
-    //  -H "Content-Type: application/json" \
-    //  -d '{"ride_type" : "lyft", "origin" : {"lat" : 37.7763, "lng" : -122.3918 } }' \
-    //  'https://api.lyft.com/v1/rides'
-		
+function authorizeApplication() {
+	var url = 'https://api.lyft.com/oauth/authorize?client_id=kK1f4CSvQe4r&scope=public%20profile%20rides.read%20rides.request%20offline&state=<state_string>&response_type=code';
+	$.ajax({
+		url: url
 	})
+	.done((res) => {
+		console.log(res); // figure out what to do for login 
+	})
+}
+
+/*
+Add event listener to button to authenticate user into app
+*/
+function insertLyftButton (eventModal) {
+	var button = document.createElement('button');
+	var buttonText = document.createTextNode('Schedule Ride');
 	button.appendChild(buttonText);	
-	eventModal[0].appendChild(button);	
+	button.addEventListener('click', requestRide);
+	eventModal[0].appendChild(button);
+	// eventModal[0].appendChild('<input type="text" placeholder="type">');	
+	authorizeApplication();
 } 
 
 // get all events on on the page and add id's on them so when we click
@@ -83,6 +56,7 @@ function load () {
 	})
 	.done(function (res) {
 		window.token = res.access_token;
+		console.log('installed')
 		console.log(context.token);
 	});
 	
@@ -94,19 +68,13 @@ function load () {
 			//async pass the modal to insertLyftButton
 			setTimeout(() => {
 				insertLyftButton(modal);
-			},1000)
+			},0)
 		})
 	};	
 }
 
-function getLyftScreen () {
-	//make request to lyft API
 
-	//display in modal
 
-};
 
-function scheduler () {
-
-};
 window.onload = load;
+
